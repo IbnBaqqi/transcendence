59060bc27e73b5304a3df22cf7b50388c188e340
Yuxin Wu <yuxin.1.wu@gmail.com>
2026-07-02 22:12:33 +0300

    removed unnecessary files and logos

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,12 @@ import './index.css'
 
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
+
 import AppRouter from "./routes";
 
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <AppRouter />
-  </React.StrictMode>
+    <React.StrictMode>
+        <AppRouter />
+    </React.StrictMode>
 );
