5c580e0e076611a45c3200a9cde86b6d964290cc
Yuxin Wu <yuxin.1.wu@gmail.com>
2026-07-02 22:51:56 +0300

    feat(frontend): add routing structure, but does not seem to work

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-import AppRouter from "./routes";
+import AppRouter from "./routes/AppRouter";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
---
+++ frontend/src/routes/AppRouter.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Profile from "../pages/Profile";
+import NotFound from "../pages/NotFound";
+
+export default function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
