60c8ae0c01733d64397654939eea15c87d701b25
Anton Kiiski <antonoliverkiiski@gmail.com>
2026-07-06 17:00:28 +0300

    restore tailwind index.cc, dedupe css import, revert lockfile churn

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,7 @@
-
 import './index.css'
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-
 import AppRouter from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
