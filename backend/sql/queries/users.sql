d22a93b49bc1c12147aa5844e890476129f09dda
Abdulbaki <Salausmart132@gmail.com>
2026-07-20 04:37:07 +0300

    feat: add queries for profile

--- backend/sql/queries/users.sql
+++ backend/sql/queries/users.sql
@@ -1,14 +1,26 @@
 -- name: GetUser :one
-SELECT * FROM users WHERE id = $1 LIMIT 1;
+SELECT * FROM users
+WHERE id = $1
+LIMIT 1;
 
 -- name: ListUsers :many
-SELECT * FROM users ORDER BY created_at DESC;
+SELECT * FROM users
+ORDER BY created_at DESC;
 
 -- name: CreateUser :one
-INSERT INTO users (email, name) VALUES ($1, $2) RETURNING *;
+INSERT INTO users (username, email, password) 
+VALUES (
+	$1, $2, $3
+)
+RETURNING *;
 
 -- name: UpdateUser :exec
-UPDATE users SET name = $2, updated_at = CURRENT_TIMESTAMP WHERE id = $1;
+UPDATE users
+SET username = $2,
+	email = $3,
+	updated_at = now()
+WHERE id = $1;
 
 -- name: DeleteUser :exec
-DELETE FROM users WHERE id = $1;
+DELETE FROM users
+WHERE id = $1;
