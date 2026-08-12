b8f1c82426d9c179d89fb12829e305d38bb353fc
Thomas Roff <tommieroff@gmail.com>
2026-08-12 12:06:06 +0300

    fix: reduced Vite server polling interval time
    N.B. see if this creates problems in performance, can be increased or
    removed later, reduced time helps with speed of iteration but may have
    side-effects
    N.B. relates to Macs OS only

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     watch: {
       usePolling: true,
-      interval: 100,
+      interval: 25,
     },
     host: true, // listen on 0.0.0.0 so the container is reachable
     port: 5173,
